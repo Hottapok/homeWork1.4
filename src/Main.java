d902ac0254846a1e32d352358c2a7e1cb40d1536
hottapok <hottapok@gmail.com>
2022-08-10 22:37:10 +0400

    Сделал ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (int a = 10; a >= 1; a--) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+
+        //task2
+
+        for (int friday = 2; friday <= 31; friday = friday + 7) {
+            System.out.println("Сегодня пятница," + friday + "-е число. Необходимо подготовить отчет.");
+        }
+
+        System.out.println();
+
+        //task3
+        int year = 2022;
+        int pastYears = year - 200;
+        int futureYears = year + 100;
+        for (int s=0;s<=futureYears;s=s+79){
+            if(s>pastYears && s<futureYears){
+                System.out.println(s);
+            }
+        }
     }
 }
